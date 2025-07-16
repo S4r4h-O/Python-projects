138cfe330049379aa1ae9fcaa45209e7a38dcf1b
Sarah <sarah@F680V9.0>
2025-07-16 20:34:13 -0300

    Add links, specify language, limit news, subject and category

--- app5-news-api/main.py
+++ app5-news-api/main.py
@@ -8,20 +8,23 @@ import send_email
 load_dotenv()
 api_key = os.getenv("API_KEY")
 
+topic = "tesla"
+
 url = ( 
     f"https://newsapi.org/v2/everything"
-    f"?q=tesla&from=2025-06-16&sortBy"
-    f"=publishedAt&apiKey={api_key}"
+    f"?q={topic}&from=2025-06-16&sortBy"
+    f"=publishedAt&apiKey={api_key}&language=en"
 )
 r = requests.get(url)
 content = r.json()
 
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     try:
-        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+        body = body + article["title"] + "\n" \
+        + article["description"] + "\n" + article["url"] + 2*"\n"
 
     except TypeError:
         pass
 
-send_email.send_email(message=body)
+send_email.send_email(message="Subject: Todays news\n" + body)
