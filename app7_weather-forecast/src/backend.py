f3830f258a57a5f76b9a28824df0a50b1f3438e5
Sarah <sarah@F680V9.0>
2025-07-20 12:27:57 -0300

    Add exception handling for ConnectionResetError

--- app7_weather-forecast/src/backend.py
+++ app7_weather-forecast/src/backend.py
@@ -2,6 +2,7 @@ import requests
 from dotenv import load_dotenv
 from os import getenv
 from typing import Optional
+import streamlit as st
 
 load_dotenv()
 api_key = getenv("API_KEY")
@@ -10,11 +11,16 @@ api_key = getenv("API_KEY")
 def get_data(place, forecast_days=None):
     url = f"https://api.openweathermap.org/data/2.5/forecast"
     params = {"q": place, "appid": api_key}
-    response = requests.get(url, params=params)
-    data = response.json()
-    filtered_data = data["list"]
-    nr_values = 8 * forecast_days
-    filtered_data = filtered_data[:nr_values]
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
+        filtered_data = data["list"]
+        nr_values = 8 * forecast_days
+        filtered_data = filtered_data[:nr_values]       
+
+    except ConnectionResetError:
+        st.write("Too many requests! Wait some time before trying again")
+
     return filtered_data
 
 
