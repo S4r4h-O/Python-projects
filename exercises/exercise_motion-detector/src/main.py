7ffe649c3adbcf5bca95855b14367f4912693dcd
Sarah <sarah@F680V9.0>
2025-07-21 16:01:11 -0300

    First commit: use datetime to show current time to the video. Each frame composes the video

---
+++ exercises/exercise_motion-detector/src/main.py
@@ -0,0 +1,28 @@
+import cv2
+import streamlit as st
+from datetime import datetime
+
+st.title("Motion Detector")
+start = st.button("Start")
+
+if start:
+    streamlit_image = st.image([])
+    camera = cv2.VideoCapture(1)
+
+    while True:
+        check, frame = camera.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        now = datetime.now()
+
+        cv2.putText(img=frame, text=now.strftime("%A"), org=(30,80),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN,fontScale=2,
+                    color=(20,100,200), thickness=2,
+                    lineType=cv2.LINE_AA)
+        cv2.putText(img=frame, text=now.strftime("%H:%M:%S"), org=(30,140),
+                    fontFace=cv2.FONT_HERSHEY_PLAIN,fontScale=2,
+                    color=(20,100,200), thickness=2,
+                    lineType=cv2.LINE_AA)
+
+
+        streamlit_image.image(frame)
