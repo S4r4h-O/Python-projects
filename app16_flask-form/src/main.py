706b10fa8b21cb26714968ffe043f60fb545ada5
Sarah <sarah.shofani@proton.me>
2025-07-30 18:28:40 -0300

    Send email with flask

--- app16_flask-form/src/main.py
+++ app16_flask-form/src/main.py
@@ -1,11 +1,25 @@
+from os import getenv
 from datetime import datetime
+from dotenv import load_dotenv
+from flask_mail import Mail, Message
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, flash
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "pythoncourse"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = getenv("EMAIL_SERVER")
+app.config["MAIL_PORT"] = int(465)
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USERNAME"] = getenv("EMAIL_ACCOUNT")
+app.config["MAIL_PASSWORD"] = getenv("EMAIL_PASSWORD")
+
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 
 class Form(db.Model):
@@ -32,13 +46,19 @@ def index():
                     email=email, date=date, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+
+        message_body = f"""Thank you for your submission, {first_name}.
+        Here's your data: {first_name}\n{last_name}\n{date}
+        """
+        message = Message(subject="New Form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+        mail.send(message)
+
         flash(f"{first_name}, your form was submitted successfully", "success")
    
     return render_template("index.html")
-
-
-
-
 
 if __name__ == "__main__":
     with app.app_context():
