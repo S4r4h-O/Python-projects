3fc88907305c0e5003b2879156f36a2eeec5332d
Sarah <sarah.shofani@proton.me>
2025-07-30 16:45:40 -0300

    Improve the template by adding values; store form data in variables

--- app16_flask-form/src/main.py
+++ app16_flask-form/src/main.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        # Retrive the form informations from the form tag
+        first_name = request.form["firstName"]
+        last_name = request.form["lastName"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
     return render_template("index.html")
 
 
