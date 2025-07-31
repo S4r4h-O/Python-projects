572ea9337b3def0948adf9b167e5fbf31a333c30
Sarah <sarah.shofani@proton.me>
2025-07-30 21:04:16 -0300

    Store data in database when form is submitted

--- app17_django-form/src/job_application/views.py
+++ app17_django-form/src/job_application/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from .models import Form
 from .forms import ApplicationForm
+from django.contrib import messages
+from django.shortcuts import render
 
 
 def index(request):
@@ -11,5 +13,9 @@ def index(request):
             email = form.cleaned_data["email"]
             date = form.cleaned_data["date"]
             occupation = form.cleaned_data["occupation"]
-            print(first_name)
+
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date, occupation=occupation)
+            messages.success(request, "Form submitted successfully!")
+            
     return render(request, "index.html")
