d0bf349a25789eb5a62e9f73d2f54531ea464ec3
Sarah <sarah.shofani@proton.me>
2025-08-03 21:06:45 -0300

    Refine models and add admin

--- DjangoCourse3/src/book_outlet/admin.py
+++ DjangoCourse3/src/book_outlet/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+import book_outlet
+from book_outlet.models import Book
 
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("rating", "author")
+    list_display = ("title", "author",)
+
+
+admin.site.register(Book, BookAdmin)
--- DjangoCourse3/src/book_outlet/models.py
+++ DjangoCourse3/src/book_outlet/models.py
@@ -4,20 +4,23 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=80)
     rating = models.IntegerField(
             validators=[MinValueValidator(0), MaxValueValidator(5)])
-    author = models.CharField(null=True, max_length=50)
+    # One to many relationship
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True)
+    slug = models.SlugField(default="", blank=True,
+                            null=False, db_index=True)
     
     def get_absolute_url(self):
         return reverse("book_detail_slug", args=[self.slug])
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
